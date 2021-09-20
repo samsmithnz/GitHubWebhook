2fe05be8d55c3af5b448c1bddb734915d540ca3e
Sam Smith <samsmithnz@gmail.com>
2021-09-20 09:12:40 -0400

    Added App insights

--- GitHubWebhook/GitHubWebhook.Service/Startup.cs
+++ GitHubWebhook/GitHubWebhook.Service/Startup.cs
@@ -27,6 +27,7 @@ namespace GitHubWebhook.Service
             });
 
             services.AddTransient<IRepo, Repo>();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
